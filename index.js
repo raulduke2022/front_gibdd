18640272e01b1e6f20e7f277a150a4e137c2b849
raulduke2022 <sergeytersahakyan@gmail.com>
2023-04-28 17:51:11 +0300

    some changes

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   let divMain = document.querySelector("div.main");
   let button = document.querySelector("button");
+  let result = document.querySelector("input[name=result]");
   button.addEventListener("click", (event) => {
     event.preventDefault();
     let input = document.querySelector("input[name=vin]").value;
     console.log(input);
-    fetch(`https://testyoursite.ru:9000/cars/`)
+    fetch(`https://testyoursite.ru:9002/cars/${input}`)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        result.value = data[0].gosnomer;
         // let { car_id, gosnomer, vin_nomer, office, model } = element;
         // divMain.innerHTML =
         //   divMain.innerHTML +
